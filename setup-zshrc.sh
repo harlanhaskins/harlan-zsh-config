b5f65def5b90c780a8a7593c07629f30c66b18e1
Harlan Haskins <harlan@harlanhaskins.com>
2014-03-31 22:10:02 -0400

    Reworded cp function to work in all cases.

--- setup-zshrc.sh
+++ setup-zshrc.sh
@@ -4,4 +4,4 @@ curl -L http://install.ohmyz.sh | sh
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone git://github.com/tarruda/zsh-autosuggestions ~/.zsh-autosuggestions
 
-cp .zshrc ~/.zshrc
+cp ~/.harlan-zsh-config/.zshrc ~/.zshrc
